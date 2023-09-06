7555263edccf039f409315465f488c17b7dd5303
Omina-Omonullaeva <aminaamanullaeva@gmail.com>
2023-09-06 14:53:11 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var elHeader = document.querySelector(".site-header")
+var elBtn = document.querySelector(".site-header__btn")
+
+elBtn.addEventListener("click", ()=> {
+    elHeader.classList.toggle("open");
+});
